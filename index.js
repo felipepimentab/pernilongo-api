619f831b8911c7bcb5dec93918ca68d761d04b51
Felipe Pimenta <felipe.pbernardo@gmail.com>
2023-06-07 15:56:18 -0300

    adds home page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const PublicationsRoute = require('./routes/pubs.js');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
 const password = process.env.PASSWORD;
+const port = process.env.PORT ? process.env.PORT : '3000'
+const dbUri = (db) => `mongodb+srv://felipepbernardo:${password}@pernilongo.6dynetw.mongodb.net/${db}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use((req, res, next) => {
@@ -14,12 +14,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const dbUri = (db) => `mongodb+srv://felipepbernardo:${password}@pernilongo.6dynetw.mongodb.net/${db}?retryWrites=true&w=majority`;
 mongoose.connect(dbUri('Publications'), {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -31,12 +30,12 @@ mongoose.connect(dbUri('Publications'), {
 .catch(err => console.log(err));
 
 // routes
-// app.get("/", (req, res) => {
-//   res.render('index')
-// });
+app.get("/", (req, res) => {
+  res.render('index')
+});
 
-app.listen(process.env.PORT, () => {
-  console.log("Listening at http://localhost:3000");
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
 })
 
 process.on('uncaughtException', function(err) {
