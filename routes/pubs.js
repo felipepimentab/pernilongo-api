c6b7c86ed479b39edaad7be5456dd7aab9391312
Felipe Pimenta <felipe.pbernardo@gmail.com>
2023-06-28 13:47:39 -0300

    fixes get(/) route

--- routes/pubs.js
+++ routes/pubs.js
@@ -4,20 +4,27 @@ const router = express.Router();
 
 // -- Get Pubs from All Topics
 router.get('/', async (req, res) => { // TO-DO: reescrever com "promise all"
-  // const currentPubs = await Current.find();
-  // const speedPubs = await Speed.find();
-  // const statePubs = await State.find();
-  // const temperaturePubs = await Temperature.find();
-  // const tensionPubs = await Tension.find();
-  // const warningPubs = await Warning.find();
-  // const pubs = {
-  //   current: currentPubs, 
-  //   speed: speedPubs,
-  //   state: statePubs,
-  //   temperature: temperaturePubs,
-  //   tension: tensionPubs,
-  //   warning: warningPubs
-  // };
+  const currentPubs = await Current.find();
+  const speedPubs = await Speed.find();
+  const statePubs = await State.find();
+  const temperaturePubs = await Temperature.find();
+  const tensionPubs = await Tension.find();
+  const warningPubs = await Warning.find();
+  const itemsPubs = await Items.find();
+  const acceptedPubs = await Accepted.find();
+  const rejectedPubs = await Rejected.find();
+
+  const pubs = {
+    current: currentPubs, 
+    speed: speedPubs,
+    state: statePubs,
+    temperature: temperaturePubs,
+    tension: tensionPubs,
+    warning: warningPubs,
+    items: itemsPubs,
+    accepted: acceptedPubs,
+    rejected: rejectedPubs
+  };
   // res.json(pubs);
   res.status(503).json({ message: 'Route temporarily unavailable.' })
 }).catch((error) => {
